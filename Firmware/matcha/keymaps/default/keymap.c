25b2d044e87f8655cc82ad801c7ff38acdb2cdb6
opyau21 <118560942+opyau21@users.noreply.github.com>
2025-05-30 16:30:20 -0700

    Added Keymap

--- Firmware/matcha/keymaps/default/keymap.c
+++ Firmware/matcha/keymaps/default/keymap.c
@@ -3,22 +3,40 @@
 
 #include QMK_KEYBOARD_H
 
+// Helpful defines
+#define GRAVE_MODS  (MOD_BIT(KC_LSFT)|MOD_BIT(KC_RSFT)|MOD_BIT(KC_LGUI)|MOD_BIT(KC_RGUI)|MOD_BIT(KC_LALT)|MOD_BIT(KC_RALT))
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * 
+    *  You can use _______ in place for KC_TRNS (transparent)   *
+    *  Or you can use XXXXXXX for KC_NO (NOOP)                  *
+    * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+// Each layer gets a name for readability.
+// The underscores don't mean anything - you can
+// have a layer called STUFF or any other name.
+// Layer names don't all need to be of the same
+// length, and you can also skip them entirely
+// and just use numbers.
+enum layer_names {
+    _BL,
+    _FL
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /*
-     * ┌───┬───┬───┬───┐
-     * │ 7 │ 8 │ 9 │ / │
-     * ├───┼───┼───┼───┤
-     * │ 4 │ 5 │ 6 │ * │
-     * ├───┼───┼───┼───┤
-     * │ 1 │ 2 │ 3 │ - │
-     * ├───┼───┼───┼───┤
-     * │ 0 │ . │Ent│ + │
-     * └───┴───┴───┴───┘
-     */
-    [0] = LAYOUT(
-        KC_P7,   KC_P8,   KC_P9,   KC_PSLS,
-        KC_P4,   KC_P5,   KC_P6,   KC_PAST,
-        KC_P1,   KC_P2,   KC_P3,   KC_PMNS,
-        KC_P0,   KC_PDOT, KC_PENT, KC_PPLS
-    )
+
+    [_BL] = LAYOUT(
+        QK_GESC,    KC_1,   KC_2,   KC_3,   KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,   KC_0,   KC_MINS,    KC_EQL, KC_BSPC,
+        KC_TAB,     KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,    KC_P,     KC_LBRC,  KC_RBRC,  KC_BSLS,
+        KC_CAPS,    KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,    KC_SCLN,  KC_QUOT,  KC_ENT,         KC_DELETE,  KC_INS,
+        KC_LSFT,    KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,   KC_SLSH,     KC_RSFT,  KC_UP,     KC+_PSCR, KC_AUDIO_MUTE,
+        KC_LCTL, KC_LGUI, KC_LALT, KC_SPACE, KC_RALT,  MO(_FL), C_LEFT, KC_DOWN, KC_RGHT
+        ),
+    
+   [_FL] = LAYOUT(
+        _______,  KC_F1,   KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,   KC_F10,   KC_F11,   KC_F12,   _______,  _______,
+        _______, _______, _______,_______,_______,_______,_______,_______,  _______,  _______,  _______,  _______,_______, _______, _______,
+        _______, _______, _______,_______,_______,_______,_______,_______,_______,_______, _______,  _______,  _______,  _______,_______,
+        _______, _______, _______,_______,_______,_______,_______,_______,_______,_______, _______,  _______,  _______,  _______, KC_SLEEP,
+        _______, _______, _______, KC_WAKE,                          _______,  MO(_FL),   _______, _______, _______
+   )
 };
